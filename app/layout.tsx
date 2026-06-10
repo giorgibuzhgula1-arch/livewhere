eb04d7989504c4677dbcd2358bdcda538948836b
Giorgi <@gmail.com>
2026-06-10 15:57:40 +0200

    Add Microsoft Clarity tracking script to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { dmSans, playfair } from '@/lib/fonts'
 import './globals.css'
 
 const GA_MEASUREMENT_ID = 'G-FZ6NK972GX'
+const CLARITY_PROJECT_ID = 'x4vqdz3sfo'
 
 export const metadata: Metadata = {
   metadataBase: new URL(getSiteUrl()),
@@ -38,6 +39,15 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', '${GA_MEASUREMENT_ID}');
+          `}
+        </Script>
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "${CLARITY_PROJECT_ID}");
           `}
         </Script>
       </head>
