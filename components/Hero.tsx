9fad3ce4ae4e76f13b73589fa43b0d84d1ec2c3d
u1447229543-art <u1447229543@gmail.com>
2026-06-25 18:41:36 +0200

    fix

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -35,7 +35,7 @@ export default function Hero({ onStart }: Props) {
       <h1
         style={{
           fontFamily: fontFamilySerif,
-          fontSize: 'clamp(47px, 7.2vw, 90px)',
+          fontSize: 'clamp(42px, 6.48vw, 81px)',
           fontWeight: 900, lineHeight: 0.95,
           letterSpacing: -2, marginBottom: 28
         }}
